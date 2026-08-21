7401d63dae2db3e6421eeeb211f64b0015fd9866
Tiago Guimarães <tiago.guimaraes@dstelecom.pt>
2026-08-21 12:43:49 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Etikra
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
